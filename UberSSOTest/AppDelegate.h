56b8381c6d8aebad037cd49477eb31eb2efb86aa
Lee Hasiuk <lee@idealab.com>
2018-01-29 16:51:20 -0500

    Initial Commit

---
+++ UberSSOTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberSSOTest
+//
+//  Created by Lee Hasiuk on 1/29/18.
+//  Copyright © 2018 Migo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UberSSOTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UberSSOTest
+//
+//  Created by Lee Hasiuk on 1/29/18.
+//  Copyright © 2018 Migo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
